ab09edfd85da34dccd370a8e2725eb6358671103
visali-93 <uvisali1993@gmail.com>
2024-11-27 12:57:53 +0530

    Add files via upload
    first week one assignment

---
+++ week 01.js
@@ -0,0 +1,23 @@
+let firstName='visali';
+let companyName='Accenture';
+let mobileNumber=8608644172;
+let isAutomation=true;
+let hasPlaywright
+console.log('my name is '+ firstName);
+console.log(typeof (firstName));
+console.log('my company Name');
+console.log(typeof(companyName));
+console.log('my mobileNumber');
+console.log(typeof(mobileNumber));
+console.log('my isAutomation');
+ console.log(typeof (isAutomation));
+console.log(hasPlaywright);
+console.log(typeof(hasplaywright));
+
+    
+    
+    
+    
+    
+    
+ 
